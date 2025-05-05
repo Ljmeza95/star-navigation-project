77f06643f8a7968b93764b6f31eacce5ab1473bb
Leo Meza <ljmeza95@gmail.com>
2025-05-04 20:08:40 -0700

    Clean up code, add comments

--- accelTest.ino
+++ accelTest.ino
@@ -4,23 +4,26 @@
 #include <Adafruit_LIS2MDL.h>
 #include <math.h>
 
-#define PI 3.14159
-
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(00001);
 Adafruit_LIS2MDL mag = Adafruit_LIS2MDL(00002);
 
 const float g = 9.81;
+const float pi = 3.14159;
+
+void updateSensors(){
+
+  
+
+}
 
 void setup(void) {
-  // put your setup code here, to run once:
+
   #ifndef ESP8266
   while (!Serial)
     ; // will pause Zero, Leonardo, etc until serial console opens
   #endif
 
-  Serial.begin(9600);
-  Serial.println("Leos Orientation Test");
-  Serial.println("");
+
 
   /* Initialise the acc sensor */
   if (!accel.begin()) {
@@ -38,40 +41,51 @@ void setup(void) {
     Serial.println("Ooops, no LIS2MDL detected ... Check your wiring!");
     while (1) delay(10);
   }
+
+  /* Starting Serial Monitor */
+  Serial.begin(9600);
+  Serial.println("Leos Orientation Test");
+  Serial.println("");
+
   pinMode(LED_BUILTIN,OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
   sensors_event_t acc_event;
   sensors_event_t mag_event;
   float pitch;
   float yaw;
+
+  //storing acceleration readings
   float xA = acc_event.acceleration.x;
   float yA = acc_event.acceleration.y;
   float zA = acc_event.acceleration.z;
 
+  //calculating pits from acceleration readings
+  accel.getEvent(&acc_event);
+  pitch = acos(zA/g)*(180/pi);
+
+  //checking for error putting zA above g, indicating pitch close to assuming no outside acceleration
+  if(zA > g) { 
+    pitch = 0;
+  }
+  //using orientation of x acceleration to correct acos() quadrant error
+  if(xA < 0){ 
+    pitch = -pitch;
+  }
+
+  //storing magnetometer readings
   float xM = mag_event.magnetic.x;
   float yM = mag_event.magnetic.y;
   float zM = mag_event.magnetic.z;
-  float absM = sqrtf(pow(xM,2.0)+pow(yM,2.0)+pow(zM,2.0));
   
-  
-
-  accel.getEvent(&acc_event);
+  //calculating yaw from magnetometer readings
   mag.getEvent(&mag_event);
-
-yaw = atan2(yM,xM)*(180/PI) + 180; 
-
-  pitch = acos(zA/g)*(180/PI);
-  if(zA > g) { //checking for error putting zA above g, indicating pitch close to 0 as long as no outside acceleration
-    pitch = 0;
-  }
-  if(xA < 0){ //using orientation of x acceleration to correct acos() quadrant error
-    pitch = -pitch;
-  }
-/* for cartesian acceleration
+  yaw = atan2(yM,xM)*(180/pi) + 180; //+180 to normalize on 0-360 doesn't work as intended, or magnetic interference CHANGE
+  
+/* serial display for cartesian acceleration
   Serial.print(" x: ");
   Serial.print(xA);
   Serial.print(" y: ");
@@ -79,7 +93,7 @@ yaw = atan2(yM,xM)*(180/PI) + 180;
   Serial.print(" z: ");
   Serial.println(zA);
 */
-  //for cartesian magetic field
+  // serial display for cartesian magetic field
   Serial.print(" x: ");
   Serial.print(xM);
   Serial.print(" y: ");
@@ -87,13 +101,13 @@ yaw = atan2(yM,xM)*(180/PI) + 180;
   Serial.print(" z: ");
   Serial.print(zM);
 
-  //for pitch angle
+  // serial display for pitch angle
   Serial.print("    Pitch: ");
   Serial.print(pitch);
   Serial.print(" Degrees");   
   delay(100);
 
-  //for yaw angle
+  // serial display for yaw angle
   Serial.print("    Yaw: ");
   Serial.print(yaw);
   Serial.println(" Degrees");   
